b6c49b95c58dcb51ed721cde75cc32c745f32f4e
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-04-12 21:02:54 +0200

    adding font and cssBaseline

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Layout from '../Layout/Layout';
+import 'typeface-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <Layout />
+    <>
+      <CssBaseline />
+      <Layout />
+    </>
   );
 }
 
