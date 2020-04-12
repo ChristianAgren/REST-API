691c1266b649a44c0e44f7053a800090cfbb3382
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-04-12 21:03:29 +0200

    working on page layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,13 +1,45 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
+import { Container, Typography, Paper, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import ComponentsList from '../Components/ComponentsList/ComponentsList';
 
+const useStyles = makeStyles((theme) => ({
+    mainContainer: {
+        minHeight: "100vh",
+        height: "100%",
+        backgroundColor: "#F5F5F5",
+    },
+    title: {
+        padding: theme.spacing(2),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 function Layout() {
+    const classes = useStyles()
 
     return (
-        <Container maxWidth="md">
-            <ComponentsList />
-        </Container>
+        <div className={classes.mainContainer}>
+            <Container maxWidth="lg">
+                <Typography className={classes.title} variant="h4">
+                    Assignments
+                    </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={4}>
+                        <Paper className={classes.paper}>Filter/info</Paper>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Paper className={classes.paper}>Generated list
+                            <ComponentsList />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
     )
 }
 
