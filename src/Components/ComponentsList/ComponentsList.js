fc547e0648f664a1b76414ae83303dd6b5bcef2e
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-04-12 21:03:10 +0200

    started working on list generation

--- src/Components/ComponentsList/ComponentsList.js
+++ src/Components/ComponentsList/ComponentsList.js
@@ -1,48 +1,69 @@
 import React from 'react'
-import { Paper, Grid } from '@material-ui/core'
+import { List, ListItem, ListItemText, ListSubheader, Typography, Divider } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+import { people } from '../../Assignments.json'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 'calc(100vh - 15rem)',
     },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+    listSection: {
+        backgroundColor: 'inherit',
+    },
+    listTitle: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: '1.1rem',
+        '& .MuiTypography-overline': {
+            color: 'rgba(0, 0, 0, 0.46)'
+        },
+    },
+    ul: {
+        backgroundColor: 'inherit',
+        padding: 0,
     },
 }));
 
 function ComponentsList() {
     const classes = useStyles()
-    
+
     return (
-            <div className={classes.root}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>xs=12</Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>xs=6</Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>xs=6</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                </Grid>
-            </div>
-    )
+        <List className={classes.root} subheader={<li />}>
+            {people.map(section => (
+                <li key={`section-${section.id}`} className={classes.listSection}>
+                    <ul className={classes.ul}>
+                            <ListSubheader color="primary" className={classes.listTitle}>
+                                {section.name} 
+                                {(section.assignments && section.assignments.length > 1) ?
+                                    <Typography variant="overline">{`Items: ${section.assignments.length}`}</Typography>
+                                    : null
+                                }
+                            </ListSubheader>
+                        {(section.assignments) ?
+                            <>
+                                {
+                                    section.assignments.map(item => (
+                                        <ListItem key={`item-${section.id}-${item.desc}`}>
+                                            <ListItemText 
+                                            primary={`Item ${item.desc}`}
+                                            secondary={`Date added: ${item.date}`}
+                                            />
+                                        </ListItem>
+                                    ))
+                                }
+                            </>
+                            : null
+                        }
+                    <Divider light component="li" />
+                    </ul>
+                </li>
+            ))}
+        </List>
+    );
 }
 
 export default ComponentsList
